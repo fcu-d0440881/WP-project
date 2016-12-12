1e740da5fcff02acffc54c71d40730535d157f4d
Nameresu-PC\Nameresu <nekosyndrome1997@gmail.com>
2016-12-12 15:17:23 +0900

    新增留言介面

---
+++ web/add.php
@@ -0,0 +1,20 @@
+<html lang="zh-TW"><head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>看評論</title>
+
+</head>
+
+<body>
+<h2>加一筆評價</h2>
+<form method="post" action="add2.php">
+學校：<input type="text" name="school" /><br>
+課名：<input type="text" name="subject" /> <br>
+授課老師：<input type="text" name="teacher" /><br>
+評價：<br>
+<textarea name="comment" cols="50" rows="15"></textarea><br>
+<input type="submit">
+
+</form>
+
+</body></html>
